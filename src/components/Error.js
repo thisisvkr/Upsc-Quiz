6184e3d6bda39f923f12b2bcba7bc9ffafff6798
Vivek Kumar <vivekkr797@gmail.com>
2024-03-26 00:03:19 +0530

    initial commit

---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+function Error() {
+  return (
+    <p className='error'>
+      <span>💥</span>There was error fetching the Server
+    </p>
+  );
+}
+
+export default Error;
---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+function Main({ childre }) {
+  return <main className='main'>{childre}</main>;
+}
+
+export default Main;
