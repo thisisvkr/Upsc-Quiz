c4622a59a0bdd07c47c82ca11d108d9a43382d7f
Vivek Kumar <vivekkr797@gmail.com>
2024-03-26 00:20:46 +0530

    set fake api
    create reducer function and fetch data from the fake api using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,36 @@
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+
 import Header from './Header';
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+
+    case 'dataFailed':
+      return { ...state, staus: 'failed' };
+    default:
+      throw new Error('unkown action');
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, 0);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/questions')
+      .then(res => res)
+      .then(data => dispatch({ type: 'dataReceived', payload: data.data }))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div>
+    <div className='app'>
       <Header />
     </div>
   );
